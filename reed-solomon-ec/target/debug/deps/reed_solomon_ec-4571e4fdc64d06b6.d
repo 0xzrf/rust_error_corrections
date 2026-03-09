4f1a93990e1eb1bcc4c155396fe98ab4a9daaa4a
0xzrf <krishh16@yahoo.com>
2026-03-09 14:21:22 +0530

    project setup

---
+++ reed-solomon-ec/target/debug/deps/reed_solomon_ec-4571e4fdc64d06b6.d
@@ -0,0 +1,9 @@
+/Users/zerefdegnl/projects/personal/rust_error_corrections/reed-solomon-ec/target/debug/deps/reed_solomon_ec-4571e4fdc64d06b6.d: src/main.rs Cargo.toml
+
+/Users/zerefdegnl/projects/personal/rust_error_corrections/reed-solomon-ec/target/debug/deps/libreed_solomon_ec-4571e4fdc64d06b6.rmeta: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
---
+++ reed-solomon-ec/target/debug/deps/reed_solomon_ec-a509d5ca53b09447.d
@@ -0,0 +1,9 @@
+/Users/zerefdegnl/projects/personal/rust_error_corrections/reed-solomon-ec/target/debug/deps/reed_solomon_ec-a509d5ca53b09447.d: src/main.rs Cargo.toml
+
+/Users/zerefdegnl/projects/personal/rust_error_corrections/reed-solomon-ec/target/debug/deps/libreed_solomon_ec-a509d5ca53b09447.rmeta: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
